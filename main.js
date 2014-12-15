3f2dbf035787d6648865ba47dba1670e8bc0336a
soutchay <chungvus@gmail.com>
2014-12-14 19:39:18 -0800

    refactor code to check for touch

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ angular.module('myApp', [])
 		link: function(scope, element){
 			//Initialize 2D drawing context
 			var ctx = element[0].getContext('2d');
-
+			var device = {};
 			//Function to find out if device has touch capabilities, returns true if has touch
 			function is_touch_device() {
 			try {
@@ -18,9 +18,21 @@ angular.module('myApp', [])
 			}
 			if (is_touch_device()){
 				console.log('has touch');
+				device.data = {
+					on: 'touchstart',
+					move: 'touchmove',
+					off: 'touchend'
+				};
+				console.log(device.data);
 			}
 			else {
 				console.log('no touch');
+				device.data = {
+					on: 'mousedown',
+					move: 'mousemove',
+					off: 'mouseup'
+				};
+				console.log(device.data);
 			}
 
 			// Drawing used to tell if currently drawing
@@ -29,22 +41,7 @@ angular.module('myApp', [])
 			var lastX;
 			var lastY;
 			//Bind the mousedown event to start drawing, takes the position of the mouse
-			element.bind('mousedown', function(event){
-				console.log(event);
-				if(event.offsetX!==undefined){
-					lastX = event.offsetX;
-					lastY = event.offsetY;
-				}
-				else {
-					lastX = event.layerX - event.currentTarget.offsetLeft;
-					lastY = event.layerY - event.currentTarget.offsetTop;
-				}
-				//Begin Line
-				ctx.beginPath();
-				drawing = true;
-			});
-
-			element.bind('touchstart', function(event){
+			element.bind(device.data.on, function(event){
 				console.log(event);
 				if(event.offsetX!==undefined){
 					lastX = event.offsetX;
@@ -60,7 +57,7 @@ angular.module('myApp', [])
 			});
 
 			//When moving mouse, will start drawing if drawing is true
-			element.bind('mousemove', function(event){
+			element.bind(device.data.move, function(event){
 				// console.log(event.layerX);
 				//Drawing is true when mousedown
 				if(drawing) {
@@ -82,49 +79,23 @@ angular.module('myApp', [])
 				}
 			});
 
-			//When moving mouse, will start drawing if drawing is true
-			element.bind('touchmove', function(event){
-				//Drawing is true when mousedown
-				if(drawing) {
-					console.log('works');
-					//get mouse position
-					if (event.offsetX!==undefined){
-						currentX = event.offsetX;
-						currentY = event.offsetY;
-					}
-					else {
-						currentX = event.layerX - event.currentTarget.offsetLeft;
-						currentY = event.layerY - event.currentTarget.offsetTop;
-					}
-
-					draw(lastX, lastY, currentX, currentY);
-					//Set current coords to last
-					lastX = currentX;
-					lastY = currentY;
-				}
-			});
 			//Give a user a random color to differentialize
 			var userColor = Math.floor(Math.random()*1000000);
 
 			//Binding mouseup to canvas to stop drawing
-			element.bind('mouseup', function(event){
+			element.bind(device.data.off, function(event){
 				ctx.strokeStyle = "#" + userColor;
 				ctx.stroke();
 				//Stop Drawing
 				drawing = false;
 			});
 
-			element.bind('touchend', function(event){
-				ctx.strokeStyle = "#" + userColor;
-				ctx.stroke();
-				//Stop Drawing
-				drawing = false;
-			});
 			//Reset Canvas Function
 			function reset(){
 				//Reset width which essentially resets the canvas
 				element[0].width = element[0].width;
 			}
+			
 			//Allows a user to draw on canvas
 			function draw(bX, bY, eX, eY ){
 				//line begins
